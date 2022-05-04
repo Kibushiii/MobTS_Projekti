ce5d5deeacc5d2b34ad1cd524cbfde6cf9e20561
okende <okende.kibushi@metropolia.fi>
2022-05-04 09:21:01 +0300

    register and login

---
+++ app/src/main/java/com/example/register_login/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.register_login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button register,login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        register = (Button) findViewById(R.id.register);
+        login = (Button) findViewById(R.id.login);
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), register.class);
+                startActivity(intent);
+            }
+        });
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), login.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
